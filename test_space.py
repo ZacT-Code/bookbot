22c533d97232aed3e883ee32e9df18c51a2c14dd
user <email@example.com>
2025-10-20 16:03:44 -0500

    update

--- test_space.py
+++ test_space.py
@@ -1,2 +1,18 @@
-import sys
-print(sys.argv)
+SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+var = [(x,y) for x in SUITS for y in RANKS]
+print(var)
